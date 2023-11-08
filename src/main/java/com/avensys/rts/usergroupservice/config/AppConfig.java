a2b7f890de2cedd727319dfeb01f4143af03aef0
rku-avensys <rahul@aven-sys.com>
2023-11-08 12:09:31 +0530

    adding token authentication

--- src/main/java/com/avensys/rts/usergroupservice/config/AppConfig.java
+++ src/main/java/com/avensys/rts/usergroupservice/config/AppConfig.java
@@ -1,44 +1,36 @@
 package com.avensys.rts.usergroupservice.config;
 
-import com.avensys.rts.usergroupservice.interceptor.AuditInterceptor;
-import com.avensys.rts.usergroupservice.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.avensys.rts.usergroupservice.interceptor.AuditInterceptor;
+import com.avensys.rts.usergroupservice.interceptor.AuthInterceptor;
 
 /**
- * @author Kotaiah nalleboina
- * This class is used to configure the application.
+ * @author Kotaiah nalleboina This class is used to configure the application.
  */
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-    /**
-     * This method is used to register the interceptors.
-     * @param registry
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuditInterceptor());
-        registry.addInterceptor(new AuthInterceptor());
-    }
+	@Autowired
+	private AuthInterceptor authInterceptor;
 
-    /**
-     * This method is used to configure the message source for internationalization.
-     * These messages are used to display error messages to the user.
-     * These messages are used to send back messages in response
-     * @return
-     */
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource ();
-        messageSource.setBasenames("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuditInterceptor());
+		registry.addInterceptor(authInterceptor);
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
 }
--- src/main/java/com/avensys/rts/usergroupservice/interceptor/AuthInterceptor.java
+++ src/main/java/com/avensys/rts/usergroupservice/interceptor/AuthInterceptor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,6 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * @author Rahul Sahu This class is used to handle JWT Auth token validation.
  */
+@Component
 public class AuthInterceptor implements HandlerInterceptor {
 
 	private final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
